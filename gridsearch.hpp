c802b5fcea22198e404fdb9e91163ca3b37d5be4
Oliver Masters <oliver.paul.masters@gmail.com>
2021-02-25 14:54:47 +0000

    Made it work on tuples

--- gridsearch.hpp
+++ gridsearch.hpp
@@ -5,99 +5,112 @@
 #include <thread>
 #include <execution>
 
-// TODO: use tuples instead of vectors
-
-template <typename T>
-void printVec(const std::vector<T>& vec)
-{
-    for(const auto& x : vec)
-        std::cout << x << ',';
-    std::cout << std::endl;
-}
-
-
 namespace gridsearch
 {
 
 namespace detail
 {
 
-inline void cartesianProductRec(std::vector<std::vector<double>>& result, std::vector<double> current, const std::vector<std::vector<double>>& factors)
+template <typename F, typename T, typename I>
+inline auto invoke_impl(F, T, I);
+
+template <typename F, typename T, std::size_t... Is>
+inline auto invoke_impl(F fun, T tup, std::index_sequence<Is...>)
 {
-    if(current.size() == factors.size())
-        return (void)result.emplace_back(std::move(current));
-    for(double x : factors[current.size()])
-    {
-        std::vector<double> neighbour = current;
-        neighbour.push_back(x);
-        cartesianProductRec(result, std::move(neighbour), factors);
-    }
+    return fun(std::get<Is>(tup)...);
 }
 
-inline std::vector<std::vector<double>> cartesianProduct(const std::vector<std::vector<double>>& factors)
+template <typename F, typename T>
+inline auto tuple_invoke(F fun, T tup)
 {
-    std::vector<std::vector<double>> res;
-    cartesianProductRec(res, {}, factors);
+    return invoke_impl(fun, tup,
+            std::make_index_sequence<std::tuple_size<T>::value>{}
+            );
+}
+
+template <int I, class... Ts>
+inline decltype(auto) get(Ts&&... ts) 
+{
+      return std::get<I>(std::forward_as_tuple(ts...));
+}
+
+template <typename T, typename... Vec>
+inline void cartesian_product_rec(std::vector<std::tuple<typename Vec::value_type...>> & result,
+        T current,
+        const Vec&... factors)
+{
+    if constexpr(std::tuple_size<T>::value == sizeof...(Vec))
+        return (void)result.emplace_back(current);
+    else
+        for(auto&& x : get<std::tuple_size<T>::value>(factors...))
+            cartesian_product_rec(result, std::tuple_cat(current, std::make_tuple(x)), factors...);
+}
+
+template <typename... Vec>
+inline auto cartesian_product(const Vec&... factors)
+{
+    std::vector<std::tuple<typename Vec::value_type...>> res;
+    cartesian_product_rec(res, std::tuple<>{}, factors...);
     return res;
 }
 
-inline std::vector<std::vector<double>> subdivideDimensions(const std::vector<double>& mins, const std::vector<double>& maxes, int subdivisions)
+template <typename T>
+inline std::vector<T> subdivide_range(T lo, T hi, int subdivisions, double prop)
 {
-    std::vector<std::vector<double>> result;
-    double prop = 1./(subdivisions - 1);
-    for(std::size_t i = 0; i < mins.size(); ++i)
-    {
-        std::vector<double> dimension_subdivision(subdivisions);
-        for(std::size_t j = 0; j < subdivisions; ++j)
-            dimension_subdivision[j] = std::lerp(mins[i], maxes[i], j * prop);
-        result.emplace_back(std::move(dimension_subdivision));
-    }
+    std::vector<T> result;
+    for(std::size_t i = 0; i < subdivisions; ++i)
+        result.emplace_back(std::lerp(lo, hi, i * prop));
     return result;
 }
 
-inline std::vector<std::vector<double>> subdivide(const std::vector<double>& mins, const std::vector<double>& maxes, int subdivisions)
+template <typename T, typename I>
+inline auto subdivide_impl(const T& mins, const T& maxes, int subdivisions, I);
+
+template <typename T, std::size_t ...Is>
+inline auto subdivide_impl(const T& mins, const T& maxes, int subdivisions, std::index_sequence<Is...>)
 {
-    std::vector<std::vector<double>> subdivision;
-
-    std::vector<std::vector<double>> dimension_subdivision = subdivideDimensions(mins, maxes, subdivisions);
-    // For example, if looking at the box [0,10] × [100,200] with subdivisions=3, this would
-    // now equal {{0,5,10},{100,150,200}}.
-
-    subdivision = cartesianProduct(std::move(dimension_subdivision));
-    // We now have the argument sets we want to run with - in the example above, arguments.front() would be {0,100}.
-    return subdivision;
-}
+    double prop = 1./(subdivisions - 1);
+    return cartesian_product(subdivide_range(std::get<Is>(mins), std::get<Is>(maxes), subdivisions, prop)...);
 }
 
-// Performs a grid search to maximise objFun.
-// Let n := mins.size() = maxes.size().  Search is performed in the box in R^n defined by
+template <typename T>
+inline auto subdivide(const T& mins, const T& maxes, int subdivisions)
+{
+    return subdivide_impl(mins, maxes, subdivisions, std::make_index_sequence<std::tuple_size<T>::value>{});
+}
+
+}
+
+// Performs a grid search to maximise fun.
+// Let n := size of mins (= size of maxes).  Search is performed in the box in R^n defined by
 // [mins[0], maxes[0]] × [mins[1], maxes[1]] × ... × [mins[n-1], maxes[n-1]].
 // concurrency is the number of threads used.
 // subdivisions must be >=2, and is the granularity of th grid. For example,
 // if subdivisions = 4, the interval [0,6] would be tested at the points {0,2,4,6}.
-template <typename F>
-auto search(F&& objFun, const std::vector<double>& mins, const std::vector<double>& maxes, int subdivisions, int concurrency)
+template <typename F, typename T>
+auto search(F fun, const T& mins, const T& maxes, int subdivisions, int concurrency)
 {
     if(subdivisions < 2)
         throw std::invalid_argument("subdivisions must be at least 2");
+    // TODO: default concurrency to hardware concurrency or something
     if(concurrency < 1)
         throw std::invalid_argument("concurrency must be at least 1");
-    if(mins.size() != maxes.size())
-        throw std::invalid_argument("mins and maxes must have equal size (" + std::to_string(mins.size()) + " vs " + std::to_string(maxes.size())  + ')');
-    const std::vector<std::vector<double>> trial_args = detail::subdivide(mins, maxes, subdivisions);
 
+    const auto trial_args = detail::subdivide(mins, maxes, subdivisions);
     using It = decltype(trial_args.begin());
     std::vector<std::pair<It, double>> arg_value_pairs;
-
     for(auto it = trial_args.begin(); it != trial_args.end(); ++it)
         arg_value_pairs.emplace_back(it, std::numeric_limits<double>::min());
 
-    std::for_each(std::execution::par, arg_value_pairs.begin(), arg_value_pairs.end(), [&](auto& arg_val){arg_val.second = objFun(*arg_val.first);});
+    std::for_each(std::execution::par,
+            arg_value_pairs.begin(),
+            arg_value_pairs.end(),
+            [&](auto& arg_val){arg_val.second = detail::tuple_invoke(fun, *arg_val.first);});
+
     auto best = std::max_element(arg_value_pairs.begin(), arg_value_pairs.end(), [](auto&& lhs, auto&& rhs){return lhs.second < rhs.second;});
-    struct { std::vector<double> args; double score;} result;
-    result.args = *(best->first);
-    result.score = best->second;
+    struct { T args; double score;} result = { .args = *(best->first), .score = best->second };
     return result;
 }
 
 }
+
--- test.cpp
+++ test.cpp
@@ -3,28 +3,40 @@
 #include <numeric>
 #include <vector>
 #include <algorithm>
+#include <thread>
+#include <tuple>
+#include <utility>
 
 #include "gridsearch.hpp"
 
 double func(double x, double y)
 {
-    using namespace std::chrono_literals;
-    std::this_thread::sleep_for(10ms);
+    // using namespace std::chrono_literals;
+    // std::this_thread::sleep_for(10ms);
     return -(std::pow((x - 1.2),2.) + std::pow((y + 0.3),2.));
 }
 
-double funcHorrible(const std::vector<double>& pts)
+template <typename T, std::size_t... Is>
+void printTupImpl(T tup, std::index_sequence<Is...>)
 {
-    return func(pts[0], pts[1]);
+    using Expander = int[];
+    (void)Expander{(std::cout << std::get<Is>(tup) << ',',0)...};
+    std::cout << std::endl;
+}
+
+template <typename T>
+void printTup(T tup)
+{
+    printTupImpl(tup, std::make_index_sequence<std::tuple_size<T>::value>{});
 }
 
 int main(void)
 {
-    std::vector<double> mins = {-2,-2};
-    std::vector<double> maxes = {2,2};
-    auto res  = gridsearch::search(funcHorrible, mins, maxes, 10, 5);
+    std::tuple<double, double> mins = {-2,-2};
+    std::tuple<double, double> maxes = {2,2};
+    auto res  = gridsearch::search(func, mins, maxes, 100, 5);
     std::cout << "best score: " << res.score << std::endl;
     std::cout << "achieve with args: ";
-    printVec(res.args);
+    printTup(res.args);
     return 0;
 }
